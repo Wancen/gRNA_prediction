8f7644c59a4defbbac014c7a7e442390064ea303
TianyouLuo <tianyou@live.unc.edu>
2022-05-05 16:46:05 -0400

    Update CNN_logFC_seqannot.py

--- CNN/log2fc/CNN_logFC_seqannot.py
+++ CNN/log2fc/CNN_logFC_seqannot.py
@@ -1,6 +1,6 @@
 import torch
 import argparse
-import os
+import os, sys
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
@@ -12,36 +12,62 @@ import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 import pandas as pd
 from scipy.stats import spearmanr
-
-parser = argparse.ArgumentParser(description='gRNA prediction model')
-parser.add_argument('--savedir', default='./', help='path to save results')
-parser.add_argument('--ckptdir', default='./ckpt', help='path to save checkpoints')
-parser.add_argument('--batch-size', type=int, default=128,
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=100,
-                    help='number of epochs to train (default: 100)')
-parser.add_argument('--lr', type=float, default=0.001,
-                    help='learning rate (default: 0.001)')
-args = parser.parse_args()
-
-savedir = args.savedir
-ckptdir = args.ckptdir
+import shap
 
 
-#batch_size = args.batch_size
-#epochs = args.epochs
-#lr = args.lr
-#ngpu=1
-
-
+datadir = '/proj/yunligrp/users/tianyou/gRNA/data/data_April_resplit/'
+resultdir = '/proj/yunligrp/users/tianyou/gRNA/result_April_resplit/'
 batch_size = 256
-epochs = 130
+epochs = 300 ## 300 for promoter winsorized, 150 for enhancer
 lr = 0.0001
 ngpu=1
+grp = 'enh'
 
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 print(device)
 
+
+#def preprocess_seq(data, strand):
+#    print("Start preprocessing the sequence")
+#    length = len(data[0])
+#    DATA_X = np.zeros((len(data),4,length), dtype=int)
+#    print(DATA_X.shape)
+#    for l in range(len(data)):
+#        if strand[l] == 1:
+#            for i in range(length):
+#                try: data[l][i]
+#                except: print(data[l], i, length, len(data))
+#                if data[l][i]in "Aa":
+#                    DATA_X[l, 0, i] = 1
+#                elif data[l][i] in "Cc":
+#                    DATA_X[l, 1, i] = 1
+#                elif data[l][i] in "Gg":
+#                    DATA_X[l, 2, i] = 1
+#                elif data[l][i] in "Tt":
+#                    DATA_X[l, 3, i] = 1
+#                else:
+#                    print("Non-ATGC character " + data[i])
+#                    sys.exit()
+#        elif strand[l] == -1:   ## negative strand: take reverse complement
+#            for i in range(length):
+#                try: data[l][i]
+#                except: print(data[l], i, length, len(data))
+#                if data[l][i]in "Aa":
+#                    DATA_X[l, 3, length - i - 1] = 1
+#                elif data[l][i] in "Cc":
+#                    DATA_X[l, 2, length - i - 1] = 1
+#                elif data[l][i] in "Gg":
+#                    DATA_X[l, 1, length - i - 1] = 1
+#                elif data[l][i] in "Tt":
+#                    DATA_X[l, 0, length - i - 1] = 1
+#                else:
+#                    print("Non-ATGC character " + data[i])
+#                    sys.exit()
+#        else:
+#            print("Invalid strand information on row " + l + ": " + strand[l])
+#            sys.exit()
+#    print("Preprocessing the sequence done")
+#    return DATA_X
 
 def preprocess_seq(data):
     print("Start preprocessing the sequence")
@@ -67,13 +93,19 @@ def preprocess_seq(data):
     return DATA_X
 
 
-dat = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-enh_rawp0.05-padj0.2-train-clean.csv', index_col = False)
+
+dat = pd.read_csv(datadir+'wgCERES-gRNAs-k562-discovery-screen-'+grp+'_logFC-train-clean.csv', index_col = False)
+
 sequence = dat['protospacer']
 sequence_onehot = preprocess_seq(sequence)
 log2FC = dat['log2FoldChange'].to_numpy(dtype = np.float32)
-#log2FC = np.abs(log2FC)
-#annotation = dat.iloc[:,13:46].to_numpy(dtype = np.float32) # for promoters
-annotation = dat.iloc[:,13:48].to_numpy(dtype = np.float32) # for enhancers
+if grp == 'pro':
+    annotation = dat.iloc[:,12:51].to_numpy(dtype = np.float32) # for promoters
+elif grp == 'enh':
+    annotation = dat.iloc[:,12:53].to_numpy(dtype = np.float32) # for enhancers
+else:
+    print("Invalid group: " + grp)
+
 
 X1 = torch.tensor(sequence_onehot, dtype=torch.float32)
 #Xloader = torch.utils.data.DataLoader(X, batch_size=batch_size, shuffle=True)
@@ -86,18 +118,26 @@ datloader = DataLoader(input_dat, batch_size=batch_size, shuffle=True)
 
 
 ## test set
-test = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-enh_rawp0.05-padj0.2-test-clean.csv', index_col = False)
+test = pd.read_csv(datadir+'/wgCERES-gRNAs-k562-discovery-screen-'+grp+'_logFC-test-clean.csv', index_col = False)
 test_sequence = test['protospacer']
 test_sequence_onehot = preprocess_seq(test_sequence)
 test_log2FC = test['log2FoldChange'].to_numpy(dtype = np.float32)
-#test_log2FC = np.abs(test_log2FC)
-#test_annotation = test.iloc[:,13:46].to_numpy(dtype = np.float32) #promoters
-test_annotation = test.iloc[:,13:48].to_numpy(dtype = np.float32) #enhancers
+if grp == 'pro':
+    test_annotation = test.iloc[:,12:51].to_numpy(dtype = np.float32) #promoters
+elif grp == "enh":
+    test_annotation = test.iloc[:,12:53].to_numpy(dtype = np.float32) #enhancers
+else:
+    print("Invalid group:" + grp)
+
 #subsample = np.random.choice(len(test_sequence), size = 3000, replace = False)
 #test_X_sub = torch.tensor(test_sequence_onehot[subsample,:], dtype=torch.float32).to(device)
 test_X1_sub = torch.tensor(test_sequence_onehot, dtype=torch.float32).to(device)
 test_X2_sub = torch.tensor(test_annotation, dtype=torch.float32).to(device)
 
+if grp == 'pro':
+    dim_fc = 139
+elif grp == 'enh':
+    dim_fc = 141
 
 class DeepSeqCNN(nn.Module):
     def __init__(self):
@@ -126,8 +166,7 @@ class DeepSeqCNN(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.3))
         self.fc2 = nn.Sequential(
-            #nn.Linear(133, 100), #promoter
-            nn.Linear(135, 100),  #enhancer
+            nn.Linear(dim_fc, 100),
             nn.ReLU(),
             nn.Dropout(0.4),
             nn.Linear(100, 80),
@@ -166,7 +205,6 @@ for epoch in range(epochs):
         CNN.eval()
         test_predict = CNN(test_X1_sub, test_X2_sub)
         test_predict_np = test_predict.detach().to('cpu').numpy()
-        #r2 = spearmanr(test_log2FC[subsample], test_predict_np)
         r2 = spearmanr(test_log2FC, test_predict_np)
         print('[%d] spearman correlation: %.3f' %
                   (epoch + 1, r2[0]))
@@ -184,11 +222,15 @@ for epoch in range(epochs):
         optimizer.step()
         
         running_loss += loss.item()
-        if i % 25 == 24:    # print every 20 mini-batches
+        if i % 25 == 24:    # print every 25 mini-batches
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 25))
             running_loss = 0.0
 
+
+ckptPATH = resultdir + '/models/gRNA_logFC-'+grp+'-MSE-seqannot-winsorize.pth'
+
+torch.save(CNN.state_dict(), ckptPATH)
 
 del test_X1_sub, test_X2_sub
 test_X1 = torch.tensor(test_sequence_onehot, dtype=torch.float32).to(device)
@@ -198,16 +240,19 @@ test_predict = CNN(test_X1, test_X2)
 test_predict_np = test_predict.detach().to('cpu').numpy()
 spearmanr(test_log2FC, test_predict_np)
 PD = pd.DataFrame(np.stack((test_log2FC, test_predict_np[:,0]), axis=1), columns = ['true', 'predict'])
-PD.to_csv("./result_deltaGB/gRNA_predict-pro-padj0.2-MSE.csv")
+PD.to_csv(resultdir + '/gRNA_logFC-'+grp+'-MSE-seqannot-winsorize.csv')
+
+
+
 
 
 ## evaluate on the other test set: promoter on enhancer
-test_oth1 = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-enh_rawp0.05-padj0.2-test-clean.csv', index_col = False)
-test_oth2 = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-enh_rawp0.05-padj0.2-train-clean.csv', index_col = False)
+test_oth1 = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-enh_rawp0.05-binary-test-clean.csv', index_col = False)
+test_oth2 = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-enh_rawp0.05-binary-train-clean.csv', index_col = False)
 test_oth = pd.concat([test_oth1,test_oth2], ignore_index=True)
 test_oth_sequence = test_oth['protospacer']
 test_oth_sequence_onehot = preprocess_seq(test_oth_sequence)
-test_oth_log2FC = test_oth['log2FoldChange'].to_numpy(dtype = np.float32)
+test_oth_label = test_oth['significant'].to_numpy(dtype = np.float32)
 #test_log2FC = np.abs(test_log2FC)
 test_oth_annotation = test_oth.iloc[:,np.r_[13:42,46,47,42,43]].to_numpy(dtype = np.float32) #for promoters, make the enhancer test file the same format
 test_oth_X1 = torch.tensor(test_oth_sequence_onehot, dtype=torch.float32).to(device)
@@ -215,27 +260,7 @@ test_oth_X2 = torch.tensor(test_oth_annotation, dtype=torch.float32).to(device)
 CNN.eval()
 test_oth_predict = CNN(test_oth_X1, test_oth_X2)
 test_oth_predict_np = test_oth_predict.detach().to('cpu').numpy()
-spearmanr(test_oth_log2FC, test_oth_predict_np)
-PD_oth = pd.DataFrame(np.stack((test_oth_log2FC, test_oth_predict_np[:,0]), axis=1), columns = ['true', 'predict'])
-PD_oth.to_csv("./result_deltaGB/gRNA_predict-pro-on-enh-padj0.2-MSE.csv")
+roc_auc_score(test_oth_label, test_oth_predict_np)
+PD_oth = pd.DataFrame(np.stack((test_oth_label, test_oth_predict_np[:,0]), axis=1), columns = ['true', 'predict'])
+PD_oth.to_csv("./result_deltaGB/gRNA_binary-pro-on-enh-BCE.csv")
 
-
-## evaluate on the other test set: enhancer on promoter
-test_oth1 = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-pro_rawp0.05-padj0.2-test-clean.csv', index_col = False)
-test_oth2 = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-pro_rawp0.05-padj0.2-train-clean.csv', index_col = False)
-test_oth = pd.concat([test_oth1,test_oth2], ignore_index=True)
-test_oth['promnumber']=np.mean(test['promnumber'])
-test_oth['promlog10fdr']=np.mean(test['promlog10fdr'])
-test_oth_sequence = test_oth['protospacer']
-test_oth_sequence_onehot = preprocess_seq(test_oth_sequence)
-test_oth_log2FC = test_oth['log2FoldChange'].to_numpy(dtype = np.float32)
-#test_log2FC = np.abs(test_log2FC)
-test_oth_annotation = test_oth.iloc[:,np.r_[13:42,44,45,47,48,42,43]].to_numpy(dtype = np.float32) #for enhancers, make the promoter test file the same format
-test_oth_X1 = torch.tensor(test_oth_sequence_onehot, dtype=torch.float32).to(device)
-test_oth_X2 = torch.tensor(test_oth_annotation, dtype=torch.float32).to(device)
-CNN.eval()
-test_oth_predict = CNN(test_oth_X1, test_oth_X2)
-test_oth_predict_np = test_oth_predict.detach().to('cpu').numpy()
-spearmanr(test_oth_log2FC, test_oth_predict_np)
-PD_oth = pd.DataFrame(np.stack((test_oth_log2FC, test_oth_predict_np[:,0]), axis=1), columns = ['true', 'predict'])
-PD_oth.to_csv("./result_deltaGB/gRNA_predict-enh-on-pro-padj0.2-MSE.csv")
