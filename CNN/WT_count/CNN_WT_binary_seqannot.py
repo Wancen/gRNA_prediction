8824d91d8c4ab4e88f0cab8676664ff72051453e
TianyouLuo <tianyou@live.unc.edu>
2022-10-04 02:24:15 -0400

    Update CNN_WT_binary_seqannot.py
    Update conv0 part to use stride 2 convolution instead; Added conv4 block for original 1d features; removed the last Sigmoid layer since BCEWithLogitsLoss already have that

--- CNN/WT_count/CNN_WT_binary_seqannot.py
+++ CNN/WT_count/CNN_WT_binary_seqannot.py
@@ -38,7 +38,7 @@ import shap
 datadir = '/proj/yunligrp/users/tianyou/gRNA/data/data_April_resplit/'
 resultdir = '/proj/yunligrp/users/tianyou/gRNA/result_April_resplit/WT_count/'
 batch_size = 256
-epochs = 2000
+epochs = 1000
 lr = 0.0001
 ngpu=1
 
@@ -104,15 +104,19 @@ test_X1_sub = torch.tensor(test_sequence_onehot, dtype=torch.float32).to(device)
 test_X2_sub = torch.tensor(test_annotation, dtype=torch.float32).to(device)
 
 
-dim_fc = 142
+dim_fc = 162
 
 class DeepSeqCNN(nn.Module):
     def __init__(self):
         super(DeepSeqCNN, self).__init__()
-        #self.input_size = input_size
+        # self.conv0 = nn.Sequential(
+        #     nn.Conv1d(4, 50, 1),
+        #     nn.MaxPool1d(2),
+        #     nn.ReLU(),
+        #     nn.Dropout(0.4),   ## for ReLU, it is interchangeable with max pooling and dropout
+        # )
         self.conv0 = nn.Sequential(
-            nn.Conv1d(4, 50, 1),
-            nn.MaxPool1d(2),
+            nn.Conv1d(4, 50, 2, stride=2),
             nn.ReLU(),
             nn.Dropout(0.4),   ## for ReLU, it is interchangeable with max pooling and dropout
         )
@@ -134,6 +138,10 @@ class DeepSeqCNN(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.4),   ## for ReLU, it is interchangeable with max pooling and dropout
         )
+        self.conv4 = nn.Sequential(
+            nn.Conv1d(4, 1, 1),
+            nn.ReLU(),
+        )
         self.fc1 = nn.Sequential(
             nn.Linear(260*10, 100),
             nn.ReLU(),
@@ -149,7 +157,7 @@ class DeepSeqCNN(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.4),
             nn.Linear(60, 1),
-            nn.Sigmoid()
+            #nn.Sigmoid()  ## BCEWithLogitsLoss takes in logits directly without sigmoid
         )
         
     def forward(self, x, y):
@@ -157,10 +165,11 @@ class DeepSeqCNN(nn.Module):
         x1 = self.conv1(x)
         x2 = self.conv2(x)
         x3 = self.conv3(x)
-        x_concat = torch.cat((x0, x1, x2, x3), dim=1) # size: [:,250,10]
+        x4 = self.conv4(x).view(-1, 20)
+        x_concat = torch.cat((x0, x1, x2, x3), dim=1) # size: [:,260,10]
         x_concat = x_concat.view(-1, 260*10)
         x_concat = self.fc1(x_concat)
-        xy_concat = torch.cat((x_concat, y), dim = 1)
+        xy_concat = torch.cat((x_concat, x4, y), dim = 1)
         xy_concat = self.fc2(xy_concat)
         #for layer in self.fc:
         #    x_concat = layer(x_concat)
@@ -173,14 +182,14 @@ CNN = DeepSeqCNN().to(device)
 optimizer = optim.Adam(CNN.parameters(), lr=lr)
 #lossfunc = nn.L1Loss().to(device)
 lossfunc = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([w])).to(device)
-
+sigmoid = nn.Sigmoid()
 
 def train_model(model, num_epochs):
     for epoch in range(num_epochs):
         # Training
         if epoch % 5 == 0:
             model.eval()
-            test_predict = model(test_X1_sub, test_X2_sub)
+            test_predict = sigmoid(model(test_X1_sub, test_X2_sub))
             test_predict_np = test_predict.detach().to('cpu').numpy()
             auc = roc_auc_score(test_label, test_predict_np)
             print('Epoch [%d] AUC: %.3f' %
@@ -207,58 +216,16 @@ def train_model(model, num_epochs):
 
 CNN = train_model(CNN, num_epochs=epochs)
 
-ckptPATH = resultdir + '/models/Maria_WT_binary-BCE-seqannot-Sep13.pth'
+ckptPATH = resultdir + '/models/Maria_WT_binary-BCE-seqannot-Oct04.pth'
 torch.save(CNN.state_dict(), ckptPATH)
 
 del test_X1_sub, test_X2_sub
 test_X1 = torch.tensor(test_sequence_onehot, dtype=torch.float32).to(device)
 test_X2 = torch.tensor(test_annotation, dtype=torch.float32).to(device)
 CNN.eval()
-test_predict = CNN(test_X1, test_X2)
+test_predict = sigmoid(CNN(test_X1, test_X2))
 test_predict_np = test_predict.detach().to('cpu').numpy()
 roc_auc_score(test_label, test_predict_np)
 PD = pd.DataFrame(np.stack((test_label, test_predict_np[:,0]), axis=1), columns = ['true', 'predict'])
 PD.to_csv(resultdir + '/Maria_WT_binary-BCE-seqannot.csv')
 
-
-
-
-
-## evaluate on the other test set: promoter on enhancer
-test_oth1 = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-enh_rawp0.05-binary-test-clean.csv', index_col = False)
-test_oth2 = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-enh_rawp0.05-binary-train-clean.csv', index_col = False)
-test_oth = pd.concat([test_oth1,test_oth2], ignore_index=True)
-test_oth_sequence = test_oth['protospacer']
-test_oth_sequence_onehot = preprocess_seq(test_oth_sequence)
-test_oth_label = test_oth['significant'].to_numpy(dtype = np.float32)
-#test_log2FC = np.abs(test_log2FC)
-test_oth_annotation = test_oth.iloc[:,np.r_[13:42,46,47,42,43]].to_numpy(dtype = np.float32) #for promoters, make the enhancer test file the same format
-test_oth_X1 = torch.tensor(test_oth_sequence_onehot, dtype=torch.float32).to(device)
-test_oth_X2 = torch.tensor(test_oth_annotation, dtype=torch.float32).to(device)
-CNN.eval()
-test_oth_predict = CNN(test_oth_X1, test_oth_X2)
-test_oth_predict_np = test_oth_predict.detach().to('cpu').numpy()
-roc_auc_score(test_oth_label, test_oth_predict_np)
-PD_oth = pd.DataFrame(np.stack((test_oth_label, test_oth_predict_np[:,0]), axis=1), columns = ['true', 'predict'])
-PD_oth.to_csv("./result_deltaGB/gRNA_binary-pro-on-enh-BCE.csv")
-
-
-## evaluate on the other test set: enhancer on promoter
-test_oth1 = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-pro_rawp0.05-binary-test-clean.csv', index_col = False)
-test_oth2 = pd.read_csv('/pine/scr/t/i/tianyou/Patrick/data/wgCERES-gRNAs-k562-discovery-screen-pro_rawp0.05-binary-train-clean.csv', index_col = False)
-test_oth = pd.concat([test_oth1,test_oth2], ignore_index=True)
-test_oth['promnumber']=np.mean(test['promnumber'])
-test_oth['promlog10fdr']=np.mean(test['promlog10fdr'])
-test_oth_sequence = test_oth['protospacer']
-test_oth_sequence_onehot = preprocess_seq(test_oth_sequence)
-test_oth_label = test_oth['significant'].to_numpy(dtype = np.float32)
-#test_log2FC = np.abs(test_log2FC)
-test_oth_annotation = test_oth.iloc[:,np.r_[13:42,44,45,47,48,42,43]].to_numpy(dtype = np.float32) #for enhancers, make the promoter test file the same format
-test_oth_X1 = torch.tensor(test_oth_sequence_onehot, dtype=torch.float32).to(device)
-test_oth_X2 = torch.tensor(test_oth_annotation, dtype=torch.float32).to(device)
-CNN.eval()
-test_oth_predict = CNN(test_oth_X1, test_oth_X2)
-test_oth_predict_np = test_oth_predict.detach().to('cpu').numpy()
-roc_auc_score(test_oth_label, test_oth_predict_np)
-PD_oth = pd.DataFrame(np.stack((test_oth_label, test_oth_predict_np[:,0]), axis=1), columns = ['true', 'predict'])
-PD_oth.to_csv("./result_deltaGB/gRNA_binary-enh-on-pro-BCE.csv")
